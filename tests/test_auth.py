6286f032c769b8c3642458a93917300aea414712
AdrianCervantes53 <adrianosohuerta@gmail.com>
2026-04-06 18:32:25 -0600

    test: add test for duplicate email registration

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -18,3 +18,23 @@ def test_register_and_login(client):
     data = response.json()
 
     assert "access_token" in data
+    
+def test_register_duplicate_email(client):
+    # Crear usuario
+    response = client.post("/users/", json={
+        "username": "testuser",
+        "email": "test@email.com",
+        "password": "12345678"
+    })
+
+    assert response.status_code == 201
+
+    # Intentar crear otro usuario con el mismo email
+    response = client.post("/users/", json={
+        "username": "testuser2",
+        "email": "test@email.com",
+        "password": "87654321"
+    })
+
+    assert response.status_code == 409
+    assert response.json()["detail"] == "Email already registered"
