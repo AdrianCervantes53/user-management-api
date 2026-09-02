c063ba5659b07d08e3517ecb7e916a524166365f
AdrianCervantes53 <adrianosohuerta@gmail.com>
2026-09-02 16:45:53 -0600

    fix: add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import note, note_share, user, auth
 
-app = FastAPI(title="User Management API")
+app = FastAPI(
+    title="User Management API",
+    description="User management api proyect",
+    version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ]
+)
 
 app.include_router(user.router)
 app.include_router(auth.router)
