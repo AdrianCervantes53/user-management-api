1b806341f3572138255b3e3c26eb82fdf1bdd00a
AdrianCervantes53 <adrianosohuerta@gmail.com>
2026-09-10 19:30:55 -0600

    fix: update CORS middleware configuration and add response model for login endpoint

--- app/main.py
+++ app/main.py
@@ -8,11 +8,17 @@ app = FastAPI(
     version="0.1.0"
 )
 
+origins = [
+        "http://localhost:5173",
+        "http://192.168.1.18:5173"
+    ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ]
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(user.router)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.dependencies.db import get_db
+from app.schemas.auth import TokenResponse
 from app.services import auth_service
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -10,6 +11,7 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 @router.post(
     "/login",
     summary="User login",
+    response_model=TokenResponse,
     description="Authenticates a user with email and password. Returns a JWT access token on success.",
 )
 def login(
